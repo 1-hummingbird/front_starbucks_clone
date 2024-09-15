d0625133c1af89922b81447718a190a9decebd95
eomhyunsik <hyunsik2005@gmail.com>
2024-09-15 18:16:38 +0900

    0915

--- src/components/mainpay/PayDelivery.tsx
+++ src/components/mainpay/PayDelivery.tsx
@@ -12,44 +12,47 @@ const data = {
 
 const PayDelivery = () => {
   return (
-    <div className="flex flex-col justify-between min-h-full">
-      <div className="flex justify-between  py-5">
-        <h4 className=" ml-9 font-bold text-xl">배송 정보</h4>
-        <div>
-          <a
-            href=""
-            className="mx-5 rounded-full border-2	 items-center text-gray-500 "
-          >
-            변경
-          </a>
-        </div>
-      </div>
-
-      <div className="ml-9 mb-5">
-        <div className="flex justify-between">
+    <>
+      <h1 className="ml-9 font-bold text-2xl	">결제하기 </h1>
+      <div className="flex flex-col justify-between min-h-full">
+        <div className="flex justify-between  py-5">
+          <h4 className=" ml-9 font-bold text-xl">배송 정보</h4>
           <div>
-            <p>
-              {data.name}({data.addressalias})
-            </p>
+            <a
+              href=""
+              className="mx-5 rounded-full border-2	 items-center text-gray-500 "
+            >
+              변경
+            </a>
+          </div>
+        </div>
 
+        <div className="ml-9 mb-5">
+          <div className="flex justify-between">
             <div>
               <p>
-                ({data.zipcode}) {data.address}({data.deepaddress})
+                {data.name}({data.addressalias})
               </p>
-            </div>
 
-            <div>
-              <p>{data.tel}</p>
-            </div>
+              <div>
+                <p>
+                  ({data.zipcode}) {data.address}({data.deepaddress})
+                </p>
+              </div>
 
-            <div>
-              <p>{data.note}</p>
+              <div>
+                <p>{data.tel}</p>
+              </div>
+
+              <div>
+                <p>{data.note}</p>
+              </div>
             </div>
           </div>
         </div>
+        <hr className=" border-border-solid border-t-[1px] z-20 border-t-slate-400" />
       </div>
-      <hr className=" border-border-solid border-t-[1px] z-20 border-t-slate-400" />
-    </div>
+    </>
   );
 };
 
