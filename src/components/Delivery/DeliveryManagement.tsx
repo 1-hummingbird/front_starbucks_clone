67465d98bca78e01fe6995217a59606ae2a15268
eomhyunsik <hyunsik2005@gmail.com>
2024-09-19 02:09:29 +0900

    0919

--- src/components/Delivery/DeliveryManagement.tsx
+++ src/components/Delivery/DeliveryManagement.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeliveryButton from "./DeliveryButton";
 
 function DeliveryManagement() {
   return (
@@ -27,11 +28,7 @@ function DeliveryManagement() {
       </div>
 
       {/* Button */}
-      <div>
-        <div className="fixed bottom-0 left-0 w-3/4 text-center bg-[#02A862] text-white py-4 rounded-full ml-9 my-4">
-          <a href="./DeliveryAddress.tsx  ">+ 새 배송지 추가</a>
-        </div>
-      </div>
+      <DeliveryButton />
     </>
   );
 }
