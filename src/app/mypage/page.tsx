cff4242e689c24269fa025991d84dfabf4f41568
eomhyunsik <hyunsik2005@gmail.com>
2024-09-18 21:05:12 +0900

    0918 mypage

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,12 @@
 import Order from "@/components/mypage/Order";
+import ServiceList from "@/components/mypage/ServiceList";
 import React from "react";
 
 function page() {
   return (
     <>
       <Order />
+      <ServiceList />
     </>
   );
 }
