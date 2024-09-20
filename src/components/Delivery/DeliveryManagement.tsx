1c0227a74621acad6fb30d6fb7cd01b4f66ec75e
eomhyunsik <hyunsik2005@gmail.com>
2024-09-20 11:56:54 +0900

    0920

--- src/components/Delivery/DeliveryManagement.tsx
+++ src/components/Delivery/DeliveryManagement.tsx
@@ -2,33 +2,56 @@ import React from "react";
 import DeliveryButton from "./DeliveryButton";
 
 function DeliveryManagement() {
+  const dummyDelivery = [
+    {
+      id: 1,
+      name: "홍길동",
+      star: "집",
+      address: "경남 김해시 장유로 000번지 000동 000호",
+      type: "기본",
+      number: "010-0000-0000",
+    },
+    {
+      id: 2,
+      name: "가나다",
+      star: "회사",
+      address: "경남 김해시 장유로 000번지 000동 000호",
+      type: null,
+      number: "010-0000-0000",
+    },
+  ];
+
   return (
     <>
-      <h2 className="p-5 text-3xl font-bold">배송지 관리</h2>
+      <h2 className="p-3 text-2xl font-bold">배송지 관리</h2>
 
-      <div className="w-4/5 px-5 leading-8">
-        <ul className="flex justify-between">
-          <li>
-            <p className="font-bold">이름</p> 주소별칭 기본배송지
-          </li>
-          <li>수정</li>
-        </ul>
-        <div>
-          <div className="flex">
-            <p>우편번호</p>
-            <p>주소</p>
-            <p>상세주소</p>
-          </div>
-          <div>
-            <p>전화번호</p>
-            <p>배송지 메모</p>
-          </div>
+      {dummyDelivery.map((delivery) => (
+        <div key={delivery.id} className="p-3">
+          <ul className="flex justify-between">
+            <div className="flex gap-2">
+              <li className="font-bold">{delivery.name}</li>
+              <li className="font-bold">({delivery.star})</li>
+              {delivery.type && (
+                <li className="text-xs italic text-green-500">
+                  {delivery.type}
+                </li>
+              )}
+            </div>
+            <div>
+              <button className="px-2 text-xs text-slate-300">수정</button>
+              <button className="px-2 text-xs text-slate-300">삭제</button>
+            </div>
+          </ul>
+          <p className="pt-1">{delivery.address}</p>
+          <p className="py-3">{delivery.number}</p>
+          <hr className="border-border-solid z-20 border-t-[1px] border-t-slate-400" />
         </div>
-        <hr className="border-border-solid z-20 border-t-[1px] border-t-slate-400" />
-      </div>
+      ))}
 
       {/* Button */}
-      <DeliveryButton />
+      <div>
+        <DeliveryButton />
+      </div>
     </>
   );
 }
