c6f7329aa2aaa0f822421acb12545847587c3e1e
eomhyunsik <hyunsik2005@gmail.com>
2024-09-20 13:47:36 +0900

    0920

--- src/components/mainpay/PayDelivery.tsx
+++ src/components/mainpay/PayDelivery.tsx
@@ -1,56 +1,33 @@
 import React from "react";
 
-const dummyDelivery = [
-  {
-    id: 1,
-    name: "홍길동",
-    star: "집",
-    address: "경남 김해시 장유로 000번지 000동 000호",
-    type: "기본",
-    number: "010-0000-0000",
-  },
-];
+const data = {
+  id: 1,
+  name: "홍길동",
+  star: "집",
+  address: "경남 김해시 장유로 000번지 000동 000호",
+  type: "기본",
+  number: "010-0000-0000",
+};
 
 const PayDelivery = () => {
   return (
     <>
-      <h1 className="ml-8 text-2xl font-bold">결제하기 </h1>
-      <div className="ml-8 flex flex-col justify-between pt-5">
-        <div className="flex justify-between py-5">
-          <h4 className="text-xl font-bold">배송 정보</h4>
-          <div>
-            <a
-              href=""
-              className="mx-5 items-center rounded-full border-2 p-1 text-gray-500"
-            >
-              변경
-            </a>
+      <h1 className="ml-7 text-2xl font-bold">결제하기 </h1>
+      <div key={data.id} className="ml-8 py-5">
+        <ul className="flex justify-between">
+          <div className="flex gap-2">
+            <li className="font-bold">{data.name}</li>
+            <li className="font-bold">({data.star})</li>
+            {data.type && (
+              <li className="text-xs italic text-green-500">{data.type}</li>
+            )}
           </div>
-        </div>
-
-        <div className="mb-5">
-          <div className="flex justify-between">
-            <div>
-              <p>
-                {data.name}({data.addressalias})
-              </p>
-
-              <div>
-                <p>
-                  ({data.zipcode}) {data.address}({data.deepaddress})
-                </p>
-              </div>
-
-              <div>
-                <p>{data.tel}</p>
-              </div>
-
-              <div>
-                <p>{data.note}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+          <button className="mr-10 rounded-xl border-2 px-1 text-slate-400">
+            변경
+          </button>
+        </ul>
+        <p className="pt-1">{data.address}</p>
+        <p className="py-3">{data.number}</p>
       </div>
       <hr className="border-border-solid z-20 border-t-[1px] border-t-slate-400" />
     </>
