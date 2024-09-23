a404ce9af6597f558ccffe3ecb78eedad643ac08
eomhyunsik <hyunsik2005@gmail.com>
2024-09-23 11:51:13 +0900

    pay 0923

--- src/components/cart/CartAdress.tsx
+++ src/components/cart/CartAdress.tsx
@@ -1,24 +1,70 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import React from "react";
+
+interface ShippingData {
+  name: string;
+  addressNickname: string;
+  address: string;
+  memo: string;
+  phone: string;
+}
 
 const CartAdress = () => {
+  const [shippingData, setShippingData] = useState<ShippingData | null>(null);
+
+  const fetchShippingData = async () => {
+    const response = await fetch(
+      "https://api.team-hummingbird.shop/api/v1/shipping/get-default",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    console.log(response);
+    console.log(setShippingData);
+
+    const data = await response.json();
+
+    //   if (data.isSuccess) {
+    //     setShippingData(data.result);
+    //   } else {
+    //     console.error("Error fetching shipping data:", data.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Error fetching shipping data:", error);
+    // }
+  };
+
+  useEffect(() => {
+    fetchShippingData();
+  }, []);
+
+  if (!shippingData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div className="bg-slate-200 p-3">
         <ul className="flex justify-between">
           <div className="flex gap-2">
-            <li className="font-bold">홍길동</li>
-            <li className="font-bold">집</li>
-            <li className="text-xs italic text-green-500">기본</li>
+            <li className="font-bold">{shippingData.name}</li>
+            <li className="font-bold">{shippingData.addressNickname}</li>
+            <li className="text-xs italic text-green-500">
+              {shippingData.memo}
+            </li>{" "}
           </div>
           <div>
             <button className="px-2 text-xs text-rose-900">
-              <Link href="cart/subadress">배송지 변경</Link>
+              <Link href="/cart/subadress">배송지 변경</Link>
             </button>
           </div>
         </ul>
-        <p className="pt-1">경상남도 김해시 000동 00호</p>
-        <p className="py-3">010-0000-0000</p>
+        <p className="pt-1">{shippingData.address}</p>
+        <p className="py-3">{shippingData.phone}</p>
       </div>
     </>
   );
--- src/components/mainpay/PayPurchase.tsx
+++ src/components/mainpay/PayPurchase.tsx
@@ -54,7 +54,7 @@ const PayPurchase = () => {
           ))}
         </div>
       </div>
-      <hr className="border-border-solid z-20 border-t-[1px] border-t-slate-400" />
+      <hr className="border-border-solid z-20 mx-6 border-t-[1px] border-t-slate-400" />
     </>
   );
 };
