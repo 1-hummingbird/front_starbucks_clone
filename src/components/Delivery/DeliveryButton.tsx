fc0a881a425320f642026c01477e84a1c5ef87e2
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-09-23 18:52:00 +0900

    :lipstick: bug resolve
    Delivery add button is sink to under of display issue occur on chrome dev mode in mobile emulation
    
    so, solve it

--- src/components/Delivery/DeliveryButton.tsx
+++ src/components/Delivery/DeliveryButton.tsx
@@ -4,7 +4,7 @@ function DeliveryButton() {
   return (
     <>
       <div>
-        <div className="fixed bottom-0 left-0 my-4 ml-9 w-3/4 rounded-full bg-[#02A862] py-4 text-center text-white">
+        <div className="fixed bottom-12 left-0 my-4 ml-9 w-3/4 rounded-full bg-[#02A862] py-4 text-center text-white">
           <a href="delivery/newdelivery">+ 새 배송지 추가</a>
         </div>
       </div>
