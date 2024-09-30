b717162fb06dcfd011a8ab7c38c2b56a614de828
rlawogns123 <evolui@naver.com>
2024-10-01 02:27:11 +0900

    :sparkles: add global not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,13 @@
 import NoutFound from '@/components/ui/NoutFound';
 import React from 'react';
 
-const NotFound = () => {
+const notFound = () => {
   return (
     <main className="flex h-[70vh] flex-col items-center justify-center">
       <NoutFound />
-      <p className="text-2xl font-bold">존재하지 않는 상품입니다.</p>
+      <p className="text-2xl font-bold">존재하지 않는 페이지입니다.</p>
     </main>
   );
 };
 
-export default NotFound;
+export default notFound;
