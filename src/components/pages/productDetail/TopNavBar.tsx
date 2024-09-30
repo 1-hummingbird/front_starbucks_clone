ca80d50ebb211adca3a566515933bc60bbfe7942
rlawogns123 <evolui@naver.com>
2024-10-01 02:22:39 +0900

    :lipstick: update TopNavBar ui on product detail page

--- src/components/pages/productDetail/TopNavBar.tsx
+++ src/components/pages/productDetail/TopNavBar.tsx
@@ -7,14 +7,32 @@ import LeftArrow from '@/components/icons/LeftArrow';
 const TopNavBar = ({ reviewCount }: { reviewCount: number }) => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
+  const [activeSection, setActiveSection] = useState<string | null>(null);
+
   useEffect(() => {
     const handleScroll = () => {
       const currentScroll = window.scrollY;
+      setIsVisible(currentScroll > 0);
 
-      if (currentScroll === 0) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
+      const productDetail = document.getElementById('productDetail');
+      const reviewList = document.getElementById('reviewList');
+
+      if (productDetail && reviewList) {
+        const productDetailPosition =
+          productDetail.getBoundingClientRect().top + currentScroll;
+        const reviewListPosition =
+          reviewList.getBoundingClientRect().top + currentScroll;
+
+        if (
+          currentScroll >= productDetailPosition - 100 &&
+          currentScroll < reviewListPosition - 100
+        ) {
+          setActiveSection('productDetail');
+        } else if (currentScroll >= reviewListPosition - 100) {
+          setActiveSection('reviewList');
+        } else {
+          setActiveSection(null);
+        }
       }
     };
 
@@ -49,19 +67,19 @@ const TopNavBar = ({ reviewCount }: { reviewCount: number }) => {
     <nav
       className={`fixed left-0 top-0 z-20 w-full transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
-      <div className="flex justify-center bg-white py-3 pr-3 shadow-lg">
+      <div className="flex justify-center bg-white py-3 shadow-lg">
         <div className="absolute left-4 top-1/2 -translate-y-1/2 transform">
           <LeftArrow />
         </div>
-        <div className="flex gap-6">
+        <div className="flex gap-4">
           <div
-            className="cursor-pointer"
+            className={`cursor-pointer ${activeSection === 'productDetail' ? 'text-green-500' : ''}`}
             onClick={() => smoothScrollTo('productDetail')}
           >
             <p>정보</p>
           </div>
           <div
-            className="cursor-pointer"
+            className={`cursor-pointer ${activeSection === 'reviewList' ? 'text-green-500' : ''}`}
             onClick={() => smoothScrollTo('reviewList')}
           >
             <p>리뷰 {reviewCount}</p>
