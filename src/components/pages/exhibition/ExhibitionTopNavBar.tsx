8e64ca4422bf0496e0c192d124c7adc0fe276dd5
rlawogns123 <evolui@naver.com>
2024-09-30 20:20:07 +0900

    :sparkles: add TopNavBar scrollIntoView on exhibition page

--- src/components/pages/exhibition/ExhibitionTopNavBar.tsx
+++ src/components/pages/exhibition/ExhibitionTopNavBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const ExhibitionTopNavBar = ({
   exhibitionTitles,
@@ -8,9 +8,14 @@ const ExhibitionTopNavBar = ({
   exhibitionTitles: string[];
 }) => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const tabRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const handleTabClick = (index: number) => {
-    setActiveIndex(index); // 클릭한 탭의 인덱스를 상태로 설정
+    setActiveIndex(index);
+    tabRefs.current[index]?.scrollIntoView({
+      behavior: 'smooth',
+      inline: 'center',
+    });
   };
   return (
     <div className="scrollbar-hide w-full overflow-x-auto whitespace-nowrap border-b-2 border-gray-300 bg-white pb-3 pt-1">
@@ -19,9 +24,12 @@ const ExhibitionTopNavBar = ({
           <div
             key={idx}
             onClick={() => handleTabClick(idx)}
+            ref={(el) => {
+              tabRefs.current[idx] = el;
+            }}
             className={`cursor-pointer px-2 text-sm font-medium ${
               activeIndex === idx ? 'text-green-500' : 'text-gray-700'
-            }`} // hover 클래스를 제거하여 클릭 시 색상이 바로 변하게 설정
+            }`}
           >
             {item}
           </div>
